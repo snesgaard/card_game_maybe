2296adf78166bce44e1e259d552ce65318d43bf8
Sebastian Jensen <s.nesgaard@gmail.com>
2022-05-04 19:03:25 +0200

    forgot

--- mechanics/card.lua
+++ mechanics/card.lua
@@ -14,4 +14,24 @@ function card_mechanics.discard(gs, user, card)
         :set(component.graveyard, user, discard:insert(card))
 end
 
+function card_mechanics.begin_card_play(gs, user, card)
+    local hand = gs:get(component.hand, user)
+    local draw = gs:get(component.draw, user)
+    local graveyard = gs:get(component.graveyard, user)
+
+    local card_being_played = gs:get(component.card_being_played, user)
+
+    local function is_not_card(c) return c ~= card end
+
+    return gs
+        :set(component.hand, hand:filter(is_not_card))
+        :set(component.draw, draw:filter(is_not_card))
+        :set(component.graveyard, graveyard:filter(is_not_card))
+        :set(component.card_being_played, card)
+end
+
+function card_mechanics.end_card_play(gs, user)
+    local graveyard = gs:get(com)
+end
+
 return card_mechanics
